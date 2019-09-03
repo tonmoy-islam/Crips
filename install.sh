fc75811ae9267e8571f9a512d611438cc1f58bd1
Tonmoy Islam Tanvir <42781990+tonmoy-islam@users.noreply.github.com>
2019-09-03 10:48:10 +0600

    Uploaded install.sh

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+clear
+echo "
+███╗   ███╗ █████╗ ███╗   ██╗██╗███████╗███████╗ ██████╗
+████╗ ████║██╔══██╗████╗  ██║██║██╔════╝██╔════╝██╔═══██╗
+██╔████╔██║███████║██╔██╗ ██║██║███████╗███████╗██║   ██║
+██║╚██╔╝██║██╔══██║██║╚██╗██║██║╚════██║╚════██║██║   ██║
+██║ ╚═╝ ██║██║  ██║██║ ╚████║██║███████║███████║╚██████╔╝
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝ 
+▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Ⓜ Ⓐ Ⓝ Ⓘ Ⓢ Ⓢ Ⓞ  ☪ ~
+  █   █  █ █  █ █   ▀▀█ 
+  ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀             
+                                                ";
+#----colors----#
+red='\033[1;31m'
+green='\033[1;32m'
+blue='\033[1;36m'
+yellow='\033[1;33m'
+
+PREFIX='/data/data/com.termux/files'
+dir='/data/data/com.termux/files/usr/share/doc/Crips/'
+
+echo "[✔] Installing ...";
+echo "";
+git clone https://github.com/tonmoy-islam/Crips.git $dir;
+echo "#!/bin/bash/ python $PREFIX/usr/share/doc/Crips/crips.py" > crips;
+cd $dir
+chmod +x crips;
+cp crips $PREFIX/usr/bin/;
+rm crips;
+
+
+if [ -d "$PREFIX/usr/share/doc/Crips" ] ;
+   then
+      echo "";
+      echo "[✔]Tool istalled with success![✔]";
+      echo "";
+      echo "[✔]====================================================================[✔]";
+      echo "[✔] ✔✔✔  All is done!! You can execute tool by typing crips  !     ✔✔✔ [✔]"; 
+      echo "[✔]====================================================================[✔]";
+      echo "";
+else
+  echo "[✘] Installation failed![✘] ";
+  exit
+fi
